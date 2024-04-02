57d3337c25c3d4697d77cfdc50b12d2442f447d0
flopwzh <phillipwzh@gmail.com>
2024-04-02 18:25:08 -0400

    create initial landing screen layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="landing-container">
+      <div className="left-side">
+        <h1>Welcome to r/gtplace</h1>
+        <div className="text-boxes">
+          <div className="box" style={{ backgroundColor: 'red' }}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse modi minus ea nostrum debitis voluptas nesciunt perspiciatis nihil adipisci, non velit mollitia, impedit, magni architecto corrupti neque nobis quaerat! Ipsam?
+          </div>
+          <div className="box" style={{ backgroundColor: 'blue' }}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto optio beatae doloremque illum quae, temporibus quo possimus impedit natus, culpa aspernatur porro iure! Error rerum molestiae ab, ut quod corrupti.
+          </div>
+          <div className="box" style={{ backgroundColor: 'green' }}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto optio beatae doloremque illum quae, temporibus quo possimus impedit natus, culpa aspernatur porro iure! Error rerum molestiae ab, ut quod corrupti.
+          </div>
+          <div className="box" style={{ backgroundColor: 'orange' }}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto optio beatae doloremque illum quae, temporibus quo possimus impedit natus, culpa aspernatur porro iure! Error rerum molestiae ab, ut quod corrupti.
+          </div>
+          <div className="box" style={{ backgroundColor: 'purple' }}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto optio beatae doloremque illum quae, temporibus quo possimus impedit natus, culpa aspernatur porro iure! Error rerum molestiae ab, ut quod corrupti.
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="right-side">
+        <button>Login</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
